9f1f44c62ee35a6c235cfdedc8665b49cdd15842
neelvshah <neel.v.shah.22@gmail.com>
2022-04-29 15:13:52 +0530

    Validation dto

--- src/main/java/com/zyapaar/projectmanagement/conroller/Ticketcontroller.java
+++ src/main/java/com/zyapaar/projectmanagement/conroller/Ticketcontroller.java
@@ -8,7 +8,4 @@ public class Ticketcontroller {
   
   TicketRepo ticketRepo;
 
-
-
-
 }
--- src/main/java/com/zyapaar/projectmanagement/dto/TicketDto.java
+++ src/main/java/com/zyapaar/projectmanagement/dto/TicketDto.java
@@ -1,6 +1,7 @@
 package com.zyapaar.projectmanagement.dto;
 
-// import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,11 +13,16 @@ public class TicketDto {
 
   // @NotNull
   // public String ticketId;
+  @NotNull(message = "Ticket Title shouldn't be empty")
+  @Size(min = 5, max = 50, message = "Description should be less than 500 characters.")
   public String ticketTitle;
+  @NotNull
+  @Size(max = 250, message = "Maximum 250 characters allowed.")
   public String ticketDescription;
   public String comment;
   // public String createdBy;
   // public String createdOn;
+  @NotNull
   public StageType stage;
   // public String workedOn;
   // public String workedBy;
